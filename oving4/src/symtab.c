66be07724a00f6d517f80237f1e3978c70278108
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-03-04 13:33:12 +0100

    Added the files handed out for problem set 4

---
+++ oving4/src/symtab.c
@@ -0,0 +1,95 @@
+#include "symtab.h"
+
+// static does not mean the same as in Java.
+// For global variables, it means they are only visible in this file.
+
+// Pointer to stack of hash tables 
+static hash_t **scopes;
+
+// Pointer to array of values, to make it easier to free them
+static symbol_t **values;
+
+// Pointer to array of strings, should be able to dynamically expand as new strings
+// are added.
+static char **strings;
+
+// Helper variables for manageing the stacks/arrays
+static int32_t scopes_size = 16, scopes_index = -1;
+static int32_t values_size = 16, values_index = -1;
+static int32_t strings_size = 16, strings_index = -1;
+
+
+void
+symtab_init ( void )
+{
+    //Dynamically allocate 3 tables with an initial size.
+}
+
+
+void
+symtab_finalize ( void )
+{
+    //Remove both tables and their contents
+}
+
+
+int32_t
+strings_add ( char *str )
+{
+    //Add str to a list of strictly growing table of all strings encountered
+    //And returns the index of the newly added string.
+}
+
+
+void
+strings_output ( FILE *stream )
+{
+    //dump all strings in the table for a given output stream.
+    //Format:
+    //.data    //fixed
+    //.INTEGER: .string "%d "
+    //.STRING0: .string "Hello, world!"
+    //.STRING1: .string "Some other thing.."
+    //.STRING<index>: .string <the text>
+    //(etc.)
+    //.globl main  //fixed
+}
+
+
+void
+scope_add ( void )
+{
+    //add hash table to stack of tables representing nested scopes.
+}
+
+
+void
+scope_remove ( void )
+{
+    //remove hash table from stack of tables representing nested scopes.
+}
+
+
+void
+symbol_insert ( char *key, symbol_t *value )
+{
+
+// Keep this for debugging/testing
+#ifdef DUMP_SYMTAB
+fprintf ( stderr, "Inserting (%s,%d)\n", key, value->stack_offset );
+#endif
+
+}
+
+
+symbol_t *
+symbol_get ( char *key )
+{
+    symbol_t* result = NULL;
+
+// Keep this for debugging/testing
+#ifdef DUMP_SYMTAB
+    if ( result != NULL )
+        fprintf ( stderr, "Retrieving (%s,%d)\n", key, result->stack_offset );
+#endif
+}
