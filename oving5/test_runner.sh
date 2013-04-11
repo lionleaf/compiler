4e09c0a859814c930f75ac20c8d742d2192cbdce
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-04-11 22:22:17 +0200

    made the tester keep the runnable compiled files!

--- oving5/test_runner.sh
+++ oving5/test_runner.sh
@@ -6,15 +6,14 @@ for inputFile in `ls vsl_programs/*.vsl`; do
 	echo "Testing $inputFile ..."
 	inputFileBase=`basename $inputFile .vsl`
 	./bin/vslc < $inputFile > testOutput/$inputFileBase.s
-	gcc -m32 testOutput/$inputFileBase.s -o testOutput/a.out
-	./testOutput/a.out > testOutput/$inputFileBase.result
+	gcc -m32 testOutput/$inputFileBase.s -o testOutput/$inputFileBase.out
+	./testOutput/$inputFileBase.out > testOutput/$inputFileBase.result
 	diff vsl_programs/$inputFileBase.output testOutput/$inputFileBase.result > testOutput/$inputFileBase.diff
 	if [ ! -s "testOutput/$inputFileBase.diff" ]; then
 		echo -e "\e[00;32mCorrect\e[00m"
-		rm testOutput/$inputFileBase.*
+		#rm testOutput/$inputFileBase.*
 	else
 		echo -e "\e[00;31mERROR\e[00m"
 	fi
-	rm testOutput/a.out
 	echo
 done
